43b6f9a09745c56a3dc8bb2dc999b1ede8ca1f7e
silvercoyote97 <haidang.levn@gmail.com>
2021-06-25 20:44:09 +0300

    Add modals

--- portfolio-page-Dang-Le/script.js
+++ portfolio-page-Dang-Le/script.js
@@ -17,3 +17,28 @@ function switchOut(event) {
     var displayedPicture = event.target
     displayedPicture.setAttribute('src', pic0);
 }
+
+
+// Modal
+function openModal(modalName) {
+    var modal = document.getElementById(modalName);
+    modal.style.display = "block";
+}
+
+function closeModal(modalName) {
+    var modal = document.getElementById(modalName);
+    modal.style.display = "none";
+}
+
+document.onclick = function(event) {
+    var prePreg = document.getElementById("prePreg");
+    var mountainBoard = document.getElementById("mountainBoard");
+    var milling = document.getElementById("milling");
+    var moldflow = document.getElementById("moldflow");
+    var printing = document.getElementById("printing");
+    var modals = document.getElementsByClassName("modal");
+    modals = [...modals]
+    if (modals.includes(event.target)) {
+        modals.forEach(m => closeModal(m.getAttribute('id')))
+    }
+}
